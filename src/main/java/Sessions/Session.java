c7511912aa83dfe4aa0497a0d55fab511c3a2374
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-07 01:14:56 -0500

    Added session package with minimum implementation done.

---
+++ src/main/java/Sessions/Session.java
@@ -0,0 +1,13 @@
+package Sessions;
+
+import java.sql.Time;
+import java.util.*;
+import java.time.*;
+
+public class Session{
+    private String course_code;
+    private String session_number;
+    private String course_name;
+    private DayOfWeek day_of_week;
+    private int year;
+}

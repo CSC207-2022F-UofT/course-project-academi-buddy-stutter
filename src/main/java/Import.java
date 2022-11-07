de8e5dc20db1d513859579c58bbde69944604461
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-07 00:57:26 -0500

    updated getSummary() in Import.

--- src/main/java/Import.java
+++ src/main/java/Import.java
@@ -1,11 +1,6 @@
 import biweekly.Biweekly;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.Time;
 import java.util.*;
 
 public class Import {
@@ -13,31 +8,82 @@ public class Import {
 
     private int student_id;
 
-    public static String readCalendar(String fileName){
-        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
-            StringBuilder builder = new StringBuilder();
-            String line = reader.readLine();
-            while (line != null) {
-                builder.append(line);
-                builder.append(System.lineSeparator());
-                line = reader.readLine();
-            }
-            return builder.toString();
-        } catch (FileNotFoundException e){
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+
     //example calendar. to be deleted
     public static void main(String[] args) {
-        System.out.println(getSummary(readCalendar("coursesCalendar.ics")));
+
+        String cal = "BEGIN:VCALENDAR\n" +
+                "PRODID:-//Ben Fortuna//iCal4j 1.0//EN\n" +
+                "VERSION:2.0\n" +
+                "CALSCALE:GREGORIAN\n" +
+                "BEGIN:VEVENT\n" +
+                "DTSTAMP:20220926T205247Z\n" +
+                "UID:08e12780-eb41-435a-a673-eb55531b6d67\n" +
+                "SUMMARY:CSC207H1 LEC0301\n" +
+                "DTSTART;TZID=America/Toronto:20220913T150000\n" +
+                "DTEND;TZID=America/Toronto:20220913T160000\n" +
+                "STATUS:CONFIRMED\n" +
+                "DESCRIPTION:Software Design\\nBrennan Hall\n" +
+                "LOCATION:BR 200\n" +
+                "CREATED:20220926T205247Z\n" +
+                "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207T235959\n" +
+                "EXDATE;TZID=America/Toronto:20221108T150000\n" +
+                "BEGIN:VALARM\n" +
+                "TRIGGER:-PT1H\n" +
+                "REPEAT:4\n" +
+                "DURATION:PT15M\n" +
+                "ACTION:DISPLAY\n" +
+                "DESCRIPTION:Software Design\n" +
+                "END:VALARM\n" +
+                "END:VEVENT\n" +
+                "BEGIN:VEVENT\n" +
+                "DTSTAMP:20220926T205247Z\n" +
+                "UID:8ae82189-568c-4de4-8404-d6864ebc924b\n" +
+                "SUMMARY:CSC207H1 LEC0301\n" +
+                "DTSTART;TZID=America/Toronto:20220908T150000\n" +
+                "DTEND;TZID=America/Toronto:20220908T160000\n" +
+                "STATUS:CONFIRMED\n" +
+                "DESCRIPTION:Software Design\\nBrennan Hall\n" +
+                "LOCATION:BR 200\n" +
+                "CREATED:20220926T205247Z\n" +
+                "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207T235959\n" +
+                "EXDATE;TZID=America/Toronto:20221110T150000\n" +
+                "BEGIN:VALARM\n" +
+                "TRIGGER:-PT1H\n" +
+                "REPEAT:4\n" +
+                "DURATION:PT15M\n" +
+                "ACTION:DISPLAY\n" +
+                "DESCRIPTION:Software Design\n" +
+                "END:VALARM\n" +
+                "END:VEVENT\n" +
+                "BEGIN:VEVENT\n" +
+                "DTSTAMP:20220926T205247Z\n" +
+                "UID:50ec0659-fe9c-49dc-baf0-be12914e1daa\n" +
+                "SUMMARY:CSC207H1 TUT0401\n" +
+                "DTSTART;TZID=America/Toronto:20220912T160000\n" +
+                "DTEND;TZID=America/Toronto:20220912T180000\n" +
+                "STATUS:CONFIRMED\n" +
+                "DESCRIPTION:Software Design\\n\n" +
+                "LOCATION: \n" +
+                "CREATED:20220926T205247Z\n" +
+                "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207T235959\n" +
+                "EXDATE;TZID=America/Toronto:20221010T160000,20221107T160000\n" +
+                "BEGIN:VALARM\n" +
+                "TRIGGER:-PT1H\n" +
+                "REPEAT:4\n" +
+                "DURATION:PT15M\n" +
+                "ACTION:DISPLAY\n" +
+                "DESCRIPTION:Software Design\n" +
+                "END:VALARM\n" +
+                "END:VEVENT\n" +
+                "END:VCALENDAR\n";
+        System.out.println(getSummary(cal));
     }
     public static List<List<String>> getSummary(String calendar) {
         /*  return a list of courses with their info.
-                  0               1               2               3                 4                 5         6
-            ["Course code", "Session type", "Session number", "Course name", "day of the week", "start time", "year"]
-            [CSC207H1, LEC, 0301, Software Design, Tue, 15:00, 2022]
+                  0               1               2               3                 4              5
+            ["Course code", "Session type", "Session number", "Course name", "day of the week", "year"]
+            [CSC207H1, LEC, 0301, Software Design, Tue, 2022]
          */
 
         List<ICalendar> icalList = Biweekly.parse(calendar).all();
@@ -47,11 +93,10 @@ public class Import {
         for(VEvent event: ical.getEvents()){
             List<String> courseInfo = new ArrayList<String>();
             courseInfo.add(event.getSummary().getValue().substring(0,8)); //Course Code
-            courseInfo.add(event.getSummary().getValue().substring(9,12)); //Sessions.Session Type
-            courseInfo.add(event.getSummary().getValue().substring(12,16)); //Sessions.Session number
+            courseInfo.add(event.getSummary().getValue().substring(9,12)); //Session Type
+            courseInfo.add(event.getSummary().getValue().substring(12,16)); //Session number
             courseInfo.add(event.getDescription().getValue().split("\n")[0]); //Course name
             courseInfo.add(event.getDateStart().getValue().toString().substring(0,3)); // day of the week
-            courseInfo.add(event.getDateStart().getValue().toString().substring(11,16)); //start time
             courseInfo.add(event.getDateStart().getValue().toString().substring(24,28)); // year
             summary.add(courseInfo);
         }
--- src/main/java/Users/Student.java
+++ src/main/java/Users/Student.java
@@ -1,18 +1,76 @@
 package Users;
-import Sessions.*;
 import java.sql.Array;
 import java.util.*;
 
-public class Student extends User{
-    private ArrayList<Tabs> tabs_of_interests;
-    private ArrayList<Course> enrolled_courses;
+public class Student {
+    String student_id;
+    private String student_password;
+    String student_info;
+    ArrayList<Tabs> tabs_of_interests;
+    //ArrayList<Course> enrolled_courses;
 
     //init
-    public Student(String UID, String UPass, String full_name, String info){
-        super(UID, UPass, full_name, info);
+    public Student(String SID, String sPass, String info){
+        this.student_id = SID;
+        this.student_password = sPass;
+        this.student_info = info;
         this.tabs_of_interests = new ArrayList<>();
-        this.enrolled_courses = new ArrayList<>();
     }
+
+    /*
+    GetStudentID: returns student id
+    param: none
+    return: String
+     */
+    public String GetStudentID(){
+        return this.student_id;
+    }
+
+    /*
+    GetStudentInfo: returns student info
+    param: none
+    return: String
+     */
+    public String GetStudentInfo(){
+        return this.student_info;
+    }
+
+    /*
+   GetStudentPassword: Get student password
+   param: none
+   return: String
+    */
+    private String GetStudentPassword(){
+       return this.student_password;
+    }
+
+    /*
+    SetStudentInfo: sets new student info
+    param: String
+    return: none
+     */
+    public void SetStudentInfo(String info){
+        this.student_info = info;
+    }
+
+    /*
+    SetStudentID: sets new student ID
+    param: String
+    return: none
+     */
+    public void SetStudentID(String ID){
+        this.student_id = ID;
+    }
+
+    /*
+    SetStudentPassword: sets new student password
+    param: String
+    return: none
+     */
+    private void SetStudentPassword(String pass){
+        this.student_password = pass;
+    }
+
 
     /*
     UpdateStudentTOI: updates the student's tabs of interest given the index of the tab required for change
@@ -23,24 +81,4 @@ public class Student extends User{
         this.tabs_of_interests.get(index).value = value;
     }
 
-    //@TODO update database for these methods
-    /*
-    public boolean addCourse(Course course){
-        if(enrolled_courses.contains(course)){
-            return false;
-        }
-        enrolled_courses.add(course);
-        course.addStudent(this);
-        return true;
-    }
-
-    public boolean removeCourse(Course course){
-        if(enrolled_courses.contains(course)){
-            enrolled_courses.remove(course);
-            course.removeStudent(this);
-            return true;
-        }
-        return false;
-    }
-    */
 }

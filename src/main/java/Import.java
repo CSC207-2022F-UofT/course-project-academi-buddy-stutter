ebf275d88754c03c68f108c84b732a8172c4fcdb
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-07 01:40:11 -0500

    Added readCalendar() and a sample courseCalendar.ics under project folder. Refined getSummary().

--- src/main/java/Import.java
+++ src/main/java/Import.java
@@ -1,6 +1,10 @@
 import biweekly.Biweekly;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 public class Import {
@@ -8,76 +12,25 @@ public class Import {
 
     private int student_id;
 
-
+    public static String readCalendar(String fileName){
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            StringBuilder builder = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null) {
+                builder.append(line);
+                builder.append(System.lineSeparator());
+                line = reader.readLine();
+            }
+            return builder.toString();
+        } catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     //example calendar. to be deleted
     public static void main(String[] args) {
-
-        String cal = "BEGIN:VCALENDAR\n" +
-                "PRODID:-//Ben Fortuna//iCal4j 1.0//EN\n" +
-                "VERSION:2.0\n" +
-                "CALSCALE:GREGORIAN\n" +
-                "BEGIN:VEVENT\n" +
-                "DTSTAMP:20220926T205247Z\n" +
-                "UID:08e12780-eb41-435a-a673-eb55531b6d67\n" +
-                "SUMMARY:CSC207H1 LEC0301\n" +
-                "DTSTART;TZID=America/Toronto:20220913T150000\n" +
-                "DTEND;TZID=America/Toronto:20220913T160000\n" +
-                "STATUS:CONFIRMED\n" +
-                "DESCRIPTION:Software Design\\nBrennan Hall\n" +
-                "LOCATION:BR 200\n" +
-                "CREATED:20220926T205247Z\n" +
-                "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207T235959\n" +
-                "EXDATE;TZID=America/Toronto:20221108T150000\n" +
-                "BEGIN:VALARM\n" +
-                "TRIGGER:-PT1H\n" +
-                "REPEAT:4\n" +
-                "DURATION:PT15M\n" +
-                "ACTION:DISPLAY\n" +
-                "DESCRIPTION:Software Design\n" +
-                "END:VALARM\n" +
-                "END:VEVENT\n" +
-                "BEGIN:VEVENT\n" +
-                "DTSTAMP:20220926T205247Z\n" +
-                "UID:8ae82189-568c-4de4-8404-d6864ebc924b\n" +
-                "SUMMARY:CSC207H1 LEC0301\n" +
-                "DTSTART;TZID=America/Toronto:20220908T150000\n" +
-                "DTEND;TZID=America/Toronto:20220908T160000\n" +
-                "STATUS:CONFIRMED\n" +
-                "DESCRIPTION:Software Design\\nBrennan Hall\n" +
-                "LOCATION:BR 200\n" +
-                "CREATED:20220926T205247Z\n" +
-                "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207T235959\n" +
-                "EXDATE;TZID=America/Toronto:20221110T150000\n" +
-                "BEGIN:VALARM\n" +
-                "TRIGGER:-PT1H\n" +
-                "REPEAT:4\n" +
-                "DURATION:PT15M\n" +
-                "ACTION:DISPLAY\n" +
-                "DESCRIPTION:Software Design\n" +
-                "END:VALARM\n" +
-                "END:VEVENT\n" +
-                "BEGIN:VEVENT\n" +
-                "DTSTAMP:20220926T205247Z\n" +
-                "UID:50ec0659-fe9c-49dc-baf0-be12914e1daa\n" +
-                "SUMMARY:CSC207H1 TUT0401\n" +
-                "DTSTART;TZID=America/Toronto:20220912T160000\n" +
-                "DTEND;TZID=America/Toronto:20220912T180000\n" +
-                "STATUS:CONFIRMED\n" +
-                "DESCRIPTION:Software Design\\n\n" +
-                "LOCATION: \n" +
-                "CREATED:20220926T205247Z\n" +
-                "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207T235959\n" +
-                "EXDATE;TZID=America/Toronto:20221010T160000,20221107T160000\n" +
-                "BEGIN:VALARM\n" +
-                "TRIGGER:-PT1H\n" +
-                "REPEAT:4\n" +
-                "DURATION:PT15M\n" +
-                "ACTION:DISPLAY\n" +
-                "DESCRIPTION:Software Design\n" +
-                "END:VALARM\n" +
-                "END:VEVENT\n" +
-                "END:VCALENDAR\n";
-        System.out.println(getSummary(cal));
+        System.out.println(getSummary(readCalendar("coursesCalendar.ics")));
     }
     public static List<List<String>> getSummary(String calendar) {
         /*  return a list of courses with their info.

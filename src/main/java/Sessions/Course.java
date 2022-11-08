19754b09a8e85b6856777508c6511ff77113dff9
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-07 19:17:19 -0500

    added course class and its methods.

---
+++ src/main/java/Sessions/Course.java
@@ -0,0 +1,65 @@
+package Sessions;
+
+import Users.Student;
+
+import java.util.*;
+import java.time.*;
+
+public class Course{
+    private String course_code;
+    private String course_type;
+    private String session_number;
+    private String course_name;
+    private DayOfWeek day_of_week;
+    private ArrayList<Student> enrolled_students;
+    private int year;
+
+    public Course(String course_code, String course_type, String session_number, String course_name, int year){
+        this.course_code = course_code;
+        this.course_type = course_type;
+        this.session_number = session_number;
+        this.course_name = course_name;
+        this.year = year;
+        enrolled_students = new ArrayList<>();
+    }
+
+    public String getCourseCode(){return course_code;}
+    public String getCourseType(){return course_type;}
+    public String getSessionNumber(){return session_number;}
+    public DayOfWeek getDayOfWeek(){return day_of_week;}
+    public int getYear(){return year;}
+    public ArrayList<Student> getEnrolledStudents(){return enrolled_students;}
+
+    public void setCourseCode(String course_code){this.course_code = course_code;}
+    public void setCourseType(String course_type){this.course_type = course_code;}
+    public void setSessionNumber(String session_number){this.session_number = course_code;}
+    public void setDayOfWeek(DayOfWeek day_of_week){this.day_of_week = day_of_week;}
+    public void setYear(int year){this.year = year;}
+    public boolean isEnrolled(Student student){
+        if(enrolled_students.contains(student)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addStudent(Student student){
+        if(isEnrolled(student)){
+            enrolled_students.add(student);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeStudent(Student student){
+        if(isEnrolled(student)){
+            enrolled_students.remove(student);
+            return true;
+        }
+        return false;
+    }
+
+    public int enrolledSize(){
+        return enrolled_students.size();
+    }
+
+}

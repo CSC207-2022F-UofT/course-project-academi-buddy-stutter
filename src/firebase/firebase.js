64f37cb6ee8f1e6fbde602b936ac7fc5f0702645
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-08 03:18:36 -0500

    modified firebase.js and package.json, successfully tested read and write capability. Need firebase access to proceed.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+//import { getAnalytics } from 'firebase/analytics';
+import { getFirestore, collection, doc, getDoc, setDoc } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,28 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firestore = getFirestore();
+//@TODO edit the path if you have access to database
+const courseDir = doc(firestore, 'courseDir/CSC207H1');
+const courseEnrollDir = doc(firestore, 'courseEnrollDir/CSC207');
+
+//TODO: add more arguments once database format is decided
+function writeCourse(code, day_of_week) {
+  var docData = {
+    course_code: code,
+    course_day_of_week: day_of_week,
+  };
+  setDoc(courseDir, docData, {merge: true});
+  console.log('successful');
+}
+/*
+function addCourse(){}
+function addStudentToCourse(student_id){}
+function removeStudentFromCourse(student_id){}
+ */
+
+writeCourse('CSC207', 'Mon');
+const courseSnap = await getDoc(courseDir);
+const courseData = courseSnap.data();
+console.log(`${JSON.stringify(courseData)}`);
+//const analytics = getAnalytics(app);

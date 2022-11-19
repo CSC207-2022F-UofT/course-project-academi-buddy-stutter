e54a226a13cc4001fd13aebdfebcebce1c48395c
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-19 16:56:47 -0500

    implemented FirebaseInterface.

--- src/main/java/Database/FirebaseCollection.java
+++ src/main/java/Database/FirebaseCollection.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class FirebaseCollection implements iFirebaseCollection {
+public class FirebaseCollection implements FirebaseInterface {
     private String collectionName;
     private Firestore db;
     public FirebaseCollection(){
--- src/main/java/Database/FirebaseInterface.java
+++ src/main/java/Database/FirebaseInterface.java
@@ -1,15 +1,11 @@
 package Database;
 
-import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
-import com.google.firebase.cloud.FirestoreClient;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
-public interface iFirebaseCollection {
+public interface FirebaseInterface {
     String collectionName = null;
     Firestore db = null;
     void initialize(String collectionName);

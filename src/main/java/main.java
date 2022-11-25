792982126165ec837fb3a7d6037b2f4376c21340
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-24 22:23:01 -0500

    Fixed a major bug in database classes.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,6 +4,7 @@ import GUI.TagMatchFrame;
 import GUI.TagSelectFrame;
 import GUI.UIController;
 import Sessions.Course;
+import Users.InterestTag;
 import Users.Student;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -11,6 +12,7 @@ import com.google.firebase.FirebaseOptions;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class main {
@@ -25,6 +27,7 @@ public class main {
     }
     public static void main(String[] args) throws IOException{
         //essential for database classes
+
         initializeFirebase();
         FirebaseCollection cbc = new FirebaseCollection();
         FirebaseCollection ubc = new FirebaseCollection();
@@ -34,9 +37,14 @@ public class main {
         TagManager tb = new TagManager(tbc, ub);
 
         Student student = new Student("12345", "qwerty", "John Doe", "A test subject");
-        UIController uiController = new UIController(student, cb, ub, tb);
-        TagSelectFrame tagSelectFrame = new TagSelectFrame(uiController);
-
+        Student student2 = new Student("23456", "qwerty", "John Doe2", "A test subject");
+        UIController uiController = new UIController(student2, cb, ub, tb);
+        //TagSelectFrame tagSelectFrame = new TagSelectFrame(uiController);
+        ub.addStudentUser(student);
+        InterestTag tag = new InterestTag("Adventure");
+        //tb.addTag(tag);
+        tb.addStudent(tag, student);
+        TagMatchFrame tagMatchFrame = new TagMatchFrame(uiController);
 
 
     }

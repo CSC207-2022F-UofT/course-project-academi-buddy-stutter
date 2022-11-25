04cc18fe21560eb4a1c65125a89707f2f1661038
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-24 19:13:33 -0500

    Updated TagSelectManager.java.

--- src/main/java/useCases/TagSelectManager.java
+++ src/main/java/useCases/TagSelectManager.java
@@ -13,9 +13,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TagSelectManager extends UseCase {
+    private TagManager tb;
 
-    public TagSelectManager(CourseManager courseDatabase, UserManager userDatabase) {
+    public TagSelectManager(CourseManager courseDatabase, UserManager userDatabase, TagManager tagManager) {
         super(courseDatabase, userDatabase);
+        this.tb= tagManager;
     }
 
+    public boolean getStudentTagState(Student self,String tagName){
+        InterestTag tag = new InterestTag(tagName);
+        return (self.isTagSelected(tag));
+    }
+
+    public void updateStudentTag(Student self, String tagName, boolean selected){
+        InterestTag tag = new InterestTag(tagName);
+        if(!tb.getTagNameList().contains(tagName)) {
+            try {
+                tb.addTag(tag);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(selected){
+            try {
+                tb.addStudent(tag, self);
+                self.updateStudentTOI(tag, true);
+                ub.addStudentUser(self);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            try {
+                tb.removeStudent(tag, self);
+                self.updateStudentTOI(tag, false);
+                ub.addStudentUser(self);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }

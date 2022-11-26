552f3c6ffc2b0ef11f5ff657b928df110580d544
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-26 17:31:14 -0500

    updated main for testing.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,18 @@
 import Database.*;
+import GUI.LoginFrame;
+import GUI.TagMatchFrame;
+import GUI.TagSelectFrame;
+import GUI.UIController;
 import Sessions.Course;
+import Users.InterestTag;
 import Users.Student;
-import GUI.*;
-import clojure.lang.IFn;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.checkerframework.checker.guieffect.qual.UI;
-
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class main {
@@ -30,32 +32,15 @@ public class main {
         FirebaseCollection cbc = new FirebaseCollection();
         FirebaseCollection ubc = new FirebaseCollection();
         FirebaseCollection tbc = new FirebaseCollection();
-
         UserManager ub = new UserManager(ubc);
         CourseManager cb = new CourseManager(cbc, ub);
         TagManager tb = new TagManager(tbc, ub);
 
-
-        //test
-        List<String> CSCInfo = CalendarInterpreter.getSummary(CalendarInterpreter.readCalendar("coursesCalendar.ics")).get(0);
-        Course CSC = new Course(CSCInfo.get(0), CSCInfo.get(1), CSCInfo.get(2), CSCInfo.get(3), CSCInfo.get(4), CSCInfo.get(5), CSCInfo.get(6));
-        Student s1 = new Student("789", "password", "s1", "none");
-        Student s2 = new Student("124", "password", "s2", "none");
-        CSC.addStudent(s1);
-        CSC.addStudent(s2);
-        cb.addCourse(CSC);
-        cb.removeStudent(CSC, s1);
-        cb.getCourse("CSC300H1");
-        ub.addUser(s1);
-        Integer i = Integer.valueOf(1);
-        System.out.println(i+1);
-
-
-        // intialize UI Controller
-        UIController uiController = new UIController(null, cb, ub, tb);
-        LoginFrame loginFrame = new LoginFrame(uiController);
-
-
+        Student student = new Student("12345", "qwerty", "John Doe", "A test subject");
+        Student student2 = new Student("23456", "qwerty", "John Doe2", "A test subject");
+        UIController uiController = new UIController(student, cb, ub, tb);
+        //TagSelectFrame tagSelectFrame = new TagSelectFrame(uiController);
+        TagMatchFrame tm = new TagMatchFrame(uiController);
 
     }
 

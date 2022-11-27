0f91fce28fb072391e9962d81b4ab1f510e420e6
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-11-27 14:59:09 -0500

    Deleted everything in main for an imminent complete rebase from branch f16.

--- src/main/java/Users/Admin.java
+++
@@ -1,8 +0,0 @@
-package Users;
-
-public class Admin extends User{
-
-    public Admin(String UID, String UPass, String full_name, String info){
-        super(UID, UPass, full_name, info);
-    }
-}

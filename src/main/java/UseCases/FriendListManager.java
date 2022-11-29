8950bc1f5b3c8217aff766fd428e50eb7ab0df6c
gheneral <ckdgh.lee@mail.utoronto.ca>
2022-11-29 02:38:36 -0500

    Friendlist params added to Student class

--- src/main/java/UseCases/FriendListManager.java
+++ src/main/java/UseCases/FriendListManager.java
@@ -1,7 +1,22 @@
 package UseCases;
 
+import Entities.Student;
+import Entities.User;
+
+import java.io.IOException;
+import java.util.List;
+
 public class FriendListManager extends UseCase{
     public FriendListManager(CourseDataManager courseDatabase, UserDataManager userDatabase) {
         super(courseDatabase, userDatabase);
+    }
+
+    public List<User> getFriendList(String userID) {
+        try {
+            Student user = (Student) this.ub.getUserByID(userID);
+            return user.getFriendList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

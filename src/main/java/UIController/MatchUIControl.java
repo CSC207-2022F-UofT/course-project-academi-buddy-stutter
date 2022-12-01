f7815eb61f535ed9ebf8e68b8d22989ef66dbd79
lob222 <93289665+lob222@users.noreply.github.com>
2022-12-01 18:27:57 -0500

    matchframe done

--- src/main/java/UIController/MatchUIControl.java
+++ src/main/java/UIController/MatchUIControl.java
@@ -6,17 +6,25 @@ import UseCases.CourseDataManager;
 import UseCases.CourseMatchManager;
 import UseCases.UserDataManager;
 
+import java.util.ArrayList;
+
 public class MatchUIControl {
     private CourseMatchManager courseMatchManager;
-    public User self;
+    public Student self;
+    private ArrayList<Student> matches = new ArrayList();
     public MatchUIControl(User self, CourseDataManager courseDatabase, UserDataManager userDatabase){
         this.self = (Student)self;
         this.courseMatchManager = new CourseMatchManager(courseDatabase, userDatabase);
     }
 
-//
-//    public getMatches(numCommon, 4){
-//        this.courseMatchManager.getTopSameSessionStudents(this.self)
-//    }
+
+    public ArrayList<Student> getMatches(int numCommon, int topNum){
+        this.matches = this.courseMatchManager.getTopSameSessionStudents(this.self, numCommon, topNum);
+        return this.matches;
+    }
+
+    public String getSelectedUserID(int index){
+        return matches.get(index).getUserID();
+    }
 
 }

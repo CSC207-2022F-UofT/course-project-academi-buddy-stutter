6f8e9e85e488be524e1584464b5b57b552ab400b
Eishieee <charliex.zhao@mail.utoronto.ca>
2022-12-03 21:12:37 -0500

    Finished javaDoc for use cases

--- src/main/java/UseCases/AllStudentsManager.java
+++ src/main/java/UseCases/AllStudentsManager.java
@@ -1,13 +1,22 @@
 package UseCases;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class AllStudentsManager extends UseCase{
+
+    /**
+     * Initiator
+     * @param courseDatabase the course database
+     * @param userDatabase the user database
+     */
     public AllStudentsManager(CourseDataManager courseDatabase, UserDataManager userDatabase) {
         super(courseDatabase, userDatabase);
     }
 
+    /**
+     * Getting all students in a user database.
+     * @return An ArrayList of Strings of user ids in the database.
+     */
     public ArrayList<String> getAllStudents() {
         System.out.println("Requesting for student list");
         return this.ub.getUserIDList();
--- src/main/java/UseCases/FriendListManager.java
+++ src/main/java/UseCases/FriendListManager.java
@@ -5,11 +5,25 @@ import Entities.Student;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Use case operations for the friend list
+ */
 public class FriendListManager extends UseCase{
+
+    /**
+     * Initializer
+     * @param courseDatabase the course database
+     * @param userDatabase the user database
+     */
     public FriendListManager(CourseDataManager courseDatabase, UserDataManager userDatabase) {
         super(courseDatabase, userDatabase);
     }
 
+    /**
+     * Get the friend list for the current user.
+     * @param userID The user id to get the friend list for
+     * @return An ArrayList of Strings of friends ids.
+     */
     public ArrayList<String> getFriendList(String userID) {
         try {
             ArrayList<String> friendList;
@@ -21,6 +35,11 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * Get the friend request list for the current user
+     * @param userID the id of the current user.
+     * @return An ArrayList of Strings of friend requests for the user.
+     */
     public ArrayList<String> getFriendRequestList(String userID) {
         try {
             ArrayList<String> friendRequestList;
@@ -32,6 +51,11 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * Gets ArrayList of strings for the friend request sent list
+     * @param userID The user's id to check
+     * @return An ArrayList of Strings of friend request sent
+     */
     public ArrayList<String> getFriendRequestSentList(String userID) {
         try {
             ArrayList<String> friendRequestSentList;
@@ -43,7 +67,11 @@ public class FriendListManager extends UseCase{
         }
     }
 
-
+    /**
+     * Gets the friend's full name.
+     * @param userID The id of the user to check
+     * @return the full name of the user.
+     */
     public String getFriendFullName(String userID) {
         try {
             return this.ub.getUserByID(userID).getFullName();
@@ -52,6 +80,12 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * To accept someone's friend request
+     * @param userId the user to accept the friend request
+     * @param friendID the friend id to accept
+     * @return True if the the friend request has been accepted, false otherwise.
+     */
     public boolean acceptFriendRequest(String userId, String friendID) {
         //Updates Student entity data
         try {
@@ -71,6 +105,11 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * Updates the database for accepting a friend request
+     * @param friendID the friend's id
+     * @param userID the user's id
+     */
     public void acceptedRequest(String friendID, String userID) {
         //Updates Student entity data
         try{
@@ -84,6 +123,11 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * Sending a friend request from user to friend.
+     * @param userID the user's id
+     * @param friendID the friend's id
+     */
     public void sendFriendRequest(String userID, String friendID) {
         // Send friend requests to friendID
         try {
@@ -96,6 +140,11 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * Receiving a friend request from friend to user.
+     * @param userID the user's id
+     * @param friendID the friend's id
+     */
     public void receiveFriendRequest(String userID, String friendID) {
         // receive friend requests to friendID
         try {
@@ -108,6 +157,12 @@ public class FriendListManager extends UseCase{
         }
     }
 
+    /**
+     * checks if the request has been sent
+     * @param userID the user's id
+     * @param friendID the friend's id
+     * @return True if the friend request has been sent successfully.
+     */
     public boolean isRequestSent(String userID, String friendID) {
         // check if friend request is sent or is friend already
         try{
@@ -115,7 +170,8 @@ public class FriendListManager extends UseCase{
             Student friend = (Student) this.ub.getUserByID(friendID);
             if (user.getFriendList().contains(friendID)) {
                 return false;
-            } else if (user.getFriendListRequest().contains(friendID) || friend.getFriendListRequest().contains(userID)) {
+            } else if (user.getFriendListRequest().contains(friendID) || friend.getFriendListRequest().contains(userID))
+            {
                 return false;
             }
             return true;

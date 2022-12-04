671f5d3c8603a4c6b6f0f9d492e42ffa2414d897
yangysu <yangy.su@mail.utoronto.ca>
2022-12-03 22:03:54 -0500

    Added Javadoc for LoginUIControl

--- src/main/java/UIController/LoginUIControl.java
+++ src/main/java/UIController/LoginUIControl.java
@@ -7,15 +7,30 @@ import Entities.User;
 
 import java.io.IOException;
 
+/**
+ * Implements LoginUIControl for LoginFrame
+ */
 public class LoginUIControl{
-
     private LoginManager loginManager;
     public User self;
+
+    /**
+     * Constructs LoginUIControl
+     * @param courseDatabase an instance of CourseDataManager
+     * @param userDatabase an instance of UserDataManager
+     */
     public LoginUIControl(CourseDataManager courseDatabase, UserDataManager userDatabase){
 
         this.loginManager = new LoginManager(courseDatabase, userDatabase);
     }
 
+    /**
+     * Try to log in with user id and password
+     * @param id user id entered by user
+     * @param password password entered by user
+     * @return whether user successfully logged in
+     * @throws IOException fails to log in
+     */
     public boolean attemptLogin(String id, String password) throws IOException {
 
         boolean loggedIn =  loginManager.login(id, password);
@@ -26,10 +41,17 @@ public class LoginUIControl{
         return loggedIn;
     }
 
+    /**
+     * set UIController's self to user
+     * @param user an instance of Student
+     */
     private void loadUser(User user){
         this.self = user;
     }
 
+    /**
+     * @return current user
+     */
     public User getUser(){
         return this.self;
     }

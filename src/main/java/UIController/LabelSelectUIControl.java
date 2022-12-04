4c9ca3ef7d1d74e3bcb3288104bcebfbf24f5ee2
yangysu <yangy.su@mail.utoronto.ca>
2022-12-03 22:00:13 -0500

    Added Javadoc for LabelSelectUIControl

--- src/main/java/UIController/LabelSelectUIControl.java
+++ src/main/java/UIController/LabelSelectUIControl.java
@@ -6,21 +6,39 @@ import UseCases.CourseDataManager;
 import UseCases.LabelSelectManager;
 import UseCases.UserDataManager;
 
+/**
+ * Implements LabelSelectUIControl
+ */
 public class LabelSelectUIControl {
     private User self;
     private LabelSelectManager labelSelectManager;
 
+    /**
+     * Constructs LabelSelectUIControl
+     * @param self a user
+     * @param courseDataManager an instance of CourseDataManager
+     * @param userDataManager an instance of UserDataManager
+     */
     public LabelSelectUIControl(User self, CourseDataManager courseDataManager, UserDataManager userDataManager){
         this.self = self;
         this.labelSelectManager = new LabelSelectManager(courseDataManager, userDataManager);
     }
 
+    /**
+     * Get user's label
+     * @param labelName name of label
+     * @return label selected by user
+     */
     public boolean getStudentLabelState(String labelName){
         return labelSelectManager.getStudentLabelState((Student) self, labelName);
     }
 
+    /**
+     * Updates user label
+     * @param labelName name of label
+     * @param selected determines whether label is selected
+     */
     public void updateStudentLabel(String labelName, boolean selected){
         labelSelectManager.updateStudentLabel((Student) self, labelName, selected);
     }
-
 }

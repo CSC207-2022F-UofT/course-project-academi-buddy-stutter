f382e9d53743aba20bf6d1a3f2480850148e9318
yangsu <yangy.su@mail.utoronto.ca>
2022-12-03 20:13:43 -0500

    Added JavaDoc to UIController

--- src/main/java/UIController/UIController.java
+++ src/main/java/UIController/UIController.java
@@ -8,7 +8,11 @@ import GUI.*;
 
 import java.io.IOException;
 
-
+/**
+ * parent class of all controllers
+ * creates instances of all controller classes
+ * creates instances of all JFrames in the gui package
+ */
 public class UIController{
 
     public int STATUS;
@@ -42,42 +46,89 @@ public class UIController{
     private AllStudentsUIControl allStudentsUIControl;
 
     private AdminUIControl adminUIControl;
+
+    /**
+     * returns adminUIControl
+     */
     public AdminUIControl getAdminUIControl() {return adminUIControl;}
 
+    /**
+     * returns loginUIControl
+     */
     public LoginUIControl getLoginUIControl() {
         return loginUIControl;
     }
 
+    /**
+     * returns registerUIControl
+     */
     public RegisterUIControl getRegisterUIControl() {
         return registerUIControl;
     }
 
+    /**
+     * returns tagMatchUIControl
+     */
     public TagMatchUIControl getTagMatchUIControl() {
         return tagMatchUIControl;
     }
 
+    /**
+     * returns tagSelectUIControl
+     */
     public TagSelectUIControl getTagSelectUIControl() {
         return tagSelectUIControl;
     }
 
+    /**
+     * returns labelSelectUIControl
+     */
     public LabelSelectUIControl getLabelSelectUIControl() {return labelSelectUIControl;}
 
+    /**
+     * returns profileUIControl
+     */
     public ProfileUIControl getProfileUIControl(){return profileUIControl;}
 
+    /**
+     * returns profileDisplayUIControl
+     */
     public ProfileDisplayUIControl getProfileDisplayUIControl(){return profileDisplayUIControl;}
 
+    /**
+     * returns homeUIControl
+     */
     public HomeUIControl getHomeUIControl(){return homeUIControl;}
 
+    /**
+     * returns friendListUIControl
+     */
     public FriendListUIControl getFriendListUIControl(){return friendListUIControl;}
 
+    /**
+     * returns matchUIControl
+     */
     public CourseMatchUIControl getMatchUIControl(){return matchUIControl;}
 
+    /**
+     * returns fileUploadUIControl
+     */
     public FileUploadUIControl getFileUploadUIControl(){return  fileUploadUIControl;}
 
+    /**
+     * returns commonSessionUIControl
+     */
     public CommonSessionUIControl getCommonSessionUIControl() {return commonSessionUIControl;}
 
     public AllStudentsUIControl getAllStudentsUIControl() {return allStudentsUIControl;}
 
+    /**
+     * Constructs a UIController with 4 parameters
+     * @param user represents a user
+     * @param courseManager instance of courseManager class
+     * @param userManager instance of userManager class
+     * @param tagManager instance of tagManager class
+     */
     public UIController(User user, CourseDataManager courseManager, UserDataManager userManager, TagDataManager tagManager){
         this.self = user;
         this.courseManager = courseManager;
@@ -85,31 +136,42 @@ public class UIController{
         this.tagManager = tagManager;
 
         // UseCases
-
-
-
         this.loginUIControl = new LoginUIControl(courseManager, userManager);
         this.registerUIControl = new RegisterUIControl(courseManager, userManager);
-
-
     }
+
+    /**
+     * updates user by calling the getUser() method from loginUIControl
+     */
     public void updateUser(){
         this.self = this.loginUIControl.getUser();
     }
 
+    /**
+     * set self user to null
+     */
     public void unloadUser() {
         this.self = null;
     }
 
+    /**
+     * returns whether the user is logged in or not
+     */
     public boolean loggedIn(){
         return this.self != null;
     }
 
+    /**
+     * takes user to LoginFrame
+     */
     public void toLogin(){
         LoginFrame loginFrame = new LoginFrame(this);
         this.unloadUser();
     }
 
+    /**
+     * takes user to HomeFrame
+     */
     public void toHome(){
         System.out.println(self.getClass());
         if(self instanceof Student){
@@ -121,53 +183,97 @@ public class UIController{
         }
     }
 
+    /**
+     * takes user to MatchFrame
+     */
     public void toMatch(){
         CourseMatchFrame matchFrame = new CourseMatchFrame(this);
     }
 
+    /**
+     * takes user to RegisterFrame
+     */
     public void toRegister(){
         RegisterFrame registerFrame = new RegisterFrame(this);
     }
 
+    /**
+     * takes user to ProfileRegFrame
+     */
     public void toRegisterProfile(){
         STATUS = FROM_REGISTER;
         ProfileRegFrame profileRegFrame = new ProfileRegFrame(this);
     }
 
+    /**
+     * takes user to ProfileFrame
+     */
     public void toProfile(){
         STATUS = FROM_PROFILE;
         ProfileFrame profileFrame = new ProfileFrame(this);
     }
 
+    /**
+     * takes user to RegCmplFrame
+     */
     public void toProfileCompleteFrame(){
         RegCmplFrame regCmplFrame = new RegCmplFrame(this);
     }
 
+    /**
+     * takes user to TagMatchFrame
+     */
     public void toTagMatch(){
         TagMatchFrame tagMatchFrame = new TagMatchFrame(this);
     }
 
+    /**
+     * takes user to TagSelectFrame
+     */
     public void toTagSelect(){
         TagSelectFrame tagSelectFrame = new TagSelectFrame(this);
     }
 
+    /**
+     * takes user to LabelSelectFrame
+     */
     public void toLabelSelect(){
         LabelSelectFrame LabelSelectFrame = new LabelSelectFrame(this);
     }
 
+    /**
+     * takes user to CalendarUploadFrame
+     */
     public void toCalendarUpload() {
         this.fileUploadUIControl = new FileUploadUIControl(self, courseManager, userManager);
         CalendarUploadFrame fileUploadFrame = new CalendarUploadFrame(this, STATUS);
     }
 
+    /**
+     * takes user to JFrame that displays userID's profile
+     * @param userID profile of this user id will be shown on the frame
+     */
     public void toProfileDisplay(String userID){ProfileDisplayFrame profileDisplayFrame = new ProfileDisplayFrame(this, userID);}
 
+    /**
+     * takes user to friendListFrame
+     */
     public void toFriendList() {FriendListFrame friendListFrame = new FriendListFrame(this);}
 
+    /**
+     * takes user to commonSessionFrame
+     * @param targetUserID shows common sessions with targetUserID
+     */
     public void toCommonSession(String targetUserID) {CommonSessionFrame commonSessionFrame = new CommonSessionFrame(this, targetUserID);}
 
+    /**
+     * takes user to AllStudentsFrame
+     */
     public void toAllStudents() {AllStudentsFrame allStudentsFrame = new AllStudentsFrame(this);}
 
+    /**
+     * initializes all controllers after user(student) logged in
+     */
     private void initializeAfterLogin(){
         this.tagMatchUIControl = new TagMatchUIControl(self, courseManager, userManager, tagManager);
         this.tagSelectUIControl = new TagSelectUIControl(self, courseManager, userManager, tagManager);
@@ -181,6 +287,9 @@ public class UIController{
         this.allStudentsUIControl = new AllStudentsUIControl(self, courseManager, userManager);
     }
 
+    /**
+     * initializes adminUIControl after an admin user logged in
+     */
     private void initializeAdminAfterLogin(){
         this.adminUIControl = new AdminUIControl(self, courseManager, userManager, tagManager);
     }

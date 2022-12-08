0f7e2dd2dc430e27fdb03cf06caf840b803ad419
gheneral <ckdgh.lee@mail.utoronto.ca>
2022-12-07 22:35:20 -0500

    Test cases updated

--- src/test/java/usecases/FriendListManagerTest.java
+++ src/test/java/usecases/FriendListManagerTest.java
@@ -5,10 +5,13 @@ import database.local.LocalCourseData;
 import database.local.LocalTagData;
 import database.local.LocalUserData;
 import model.usecases.FriendListManager;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Executable;
 import java.util.ArrayList;
 
 
@@ -31,39 +34,58 @@ class FriendListManagerTest extends LocalTempDataFactory {
     FriendListManager friendListManager = new FriendListManager(cb, ub, tb);
 
     @Test
-    void acceptFriendRequest() throws Exception {
-        ArrayList<String> friendAList = STUDENTA.getFriendList();
-        ArrayList<String> friendCList = STUDENTC.getFriendList();
-
-        // Send friend request and accept
-        STUDENTC.sendFriendRequest(STUDENTA.getUserID());
-        STUDENTA.acceptFriendRequest(STUDENTC.getUserID());
-        friendAList.add(STUDENTC.getUserID());
+    void acceptAndUpdateFriendRequestTest() throws Exception {
+        ArrayList<String> friendAList = new ArrayList<>();
+        ArrayList<String> friendCList = new ArrayList<>();
+        friendAList.add(STUDENTB.getUserID());
         friendCList.add(STUDENTA.getUserID());
+        System.out.println(friendCList);
 
-        ArrayList<String> updatedFriendAList = STUDENTA.getFriendList();
-        ArrayList<String> updatedFriendCList = STUDENTC.getFriendList();
+        // C is inside request list
+        Assertions.assertEquals(friendAList, friendListManager.getFriendList(STUDENTA.getUserID()));
+        // A is inside request sent list
+        Assertions.assertEquals(friendCList, friendListManager.getFriendRequestSentList(STUDENTC.getUserID()));
 
-        Assertions.assertEquals(friendAList, updatedFriendAList);
-        Assertions.assertEquals(friendCList, updatedFriendCList);
+        // accept friend request and update both Student's friend list
+        friendListManager.acceptFriendRequest(STUDENTA.getUserID(), STUDENTC.getUserID());
+        friendListManager.acceptedRequest(STUDENTC.getUserID(), STUDENTA.getUserID());
+
+        // friend list has been updated
+        friendAList.add(STUDENTC.getUserID());
+        friendCList.remove(STUDENTA.getUserID());
+        Assertions.assertEquals(friendAList, friendListManager.getFriendList(STUDENTA.getUserID()));
+        Assertions.assertEquals(friendCList, friendListManager.getFriendRequestSentList(STUDENTC.getUserID()));
+
+        // both request and request sent list has been cleared
+        Assertions.assertEquals(new ArrayList<>(), friendListManager.getFriendRequestList(STUDENTA.getUserID()));
+        Assertions.assertEquals(new ArrayList<>(), friendListManager.getFriendRequestSentList(STUDENTC.getUserID()));
     }
 
     @Test
-    void sendFriendRequest() throws Exception {
-        ArrayList<String> friendListB = STUDENTB.getFriendList();
-        ArrayList<String> friendListC = STUDENTC.getFriendList();
+    void sendAndReceiveFriendRequestTest() throws Exception {
+        ArrayList<String> friendListB = new ArrayList<>();
+        ArrayList<String> friendListC = new ArrayList<>();
 
-        STUDENTB.sendFriendRequest(STUDENTC.getUserID());
-        STUDENTC.receiveFriendRequest(STUDENTB.getUserID());
-        STUDENTC.acceptFriendRequest(STUDENTB.getUserID());
+        // Check no friend request has been sent for B and C
+        Assertions.assertEquals(friendListB, friendListManager.getFriendRequestSentList(STUDENTB.getUserID()));
+        Assertions.assertEquals(friendListC, friendListManager.getFriendRequestList(STUDENTC.getUserID()));
 
-        ArrayList<String> updatedFriendListB = STUDENTB.getFriendList();
-        ArrayList<String> updatedFriendListC = STUDENTC.getFriendList();
+        // B send friend request to C
+        friendListManager.sendFriendRequest(STUDENTB.getUserID(), STUDENTC.getUserID());
+        friendListManager.receiveFriendRequest(STUDENTB.getUserID(), STUDENTC.getUserID());
 
+        // Check B and C's friend request sent and request list
         friendListB.add(STUDENTC.getUserID());
         friendListC.add(STUDENTB.getUserID());
-
-        Assertions.assertEquals(friendListB, updatedFriendListB);
-        Assertions.assertEquals(friendListC, updatedFriendListC);
+        Assertions.assertEquals(friendListB, friendListManager.getFriendRequestSentList(STUDENTB.getUserID()));
+        Assertions.assertEquals(friendListC, friendListManager.getFriendRequestList(STUDENTC.getUserID()));
     }
+
+    @Test
+    public void isRequestSentTest() {
+        Assertions.assertFalse(friendListManager.isRequestSent(STUDENTA.getUserID(), STUDENTC.getUserID()));
+        Assertions.assertFalse(friendListManager.isRequestSent(STUDENTA.getUserID(), STUDENTB.getUserID()));
+        Assertions.assertTrue(friendListManager.isRequestSent(STUDENTB.getUserID(), STUDENTC.getUserID()));
+    }
+
 }
